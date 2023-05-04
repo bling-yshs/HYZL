7a188f40870396cfc4303d30c9fd9369330dee4f
bling_yshs <mcyishan@outlook.com>
2023-05-04 09:54:32 +0800

    feat: 增加星穹铁道插件安装 fix: 逍遥插件安装判断错误

--- install_plugins.go
+++ install_plugins.go
@@ -14,6 +14,7 @@ func installPluginsMenu() {
 		fmt.Println("2. 喵喵插件")
 		fmt.Println("3. 逍遥插件")
 		fmt.Println("4. 枫叶插件")
+		fmt.Println("5. 星穹铁道插件")
 		fmt.Println("0. 返回上一级")
 		fmt.Print("\n请选择操作：")
 		var choice int
@@ -40,6 +41,9 @@ func installPluginsMenu() {
 		case 4:
 			clearLog()
 			installFengyePlugin()
+		case 5:
+			clearLog()
+			installStarRailPlugin()
 		default:
 			printWithEmptyLine("选择不正确，请重新选择")
 		}
@@ -47,6 +51,10 @@ func installPluginsMenu() {
 }
 
 //↓插件安装函数
+
+func installStarRailPlugin() {
+	installPluginsTemplate("星穹铁道插件", "StarRail-plugin", "git clone --depth=1 https://gitee.com/hewang1an/StarRail-plugin.git ./plugins/StarRail-plugin/")
+}
 
 func installGuobaPlugin() {
 	installPluginsTemplate("锅巴插件", "Guoba-Plugin", "git clone --depth=1 https://gitee.com/guoba-yunzai/guoba-plugin.git ./plugins/Guoba-Plugin/", "pnpm install --no-lockfile --filter=guoba-plugin -w")
@@ -57,11 +65,11 @@ func installMiaoPlugin() {
 }
 
 func installXiaoyaoPlugin() {
-	installPluginsTemplate("逍遥插件", "miao-plugin", "git clone --depth=1 https://gitee.com/Ctrlcvs/xiaoyao-cvs-plugin.git ./plugins/xiaoyao-cvs-plugin/ ./plugins/xiaoyao-cvs-plugin/", "pnpm add promise-retry -w", "pnpm add superagent -w")
+	installPluginsTemplate("逍遥插件", "xiaoyao-cvs-plugin", "git clone --depth=1 https://gitee.com/Ctrlcvs/xiaoyao-cvs-plugin.git ./plugins/xiaoyao-cvs-plugin/ ./plugins/xiaoyao-cvs-plugin/", "pnpm add promise-retry -w", "pnpm add superagent -w")
 }
 
 func installFengyePlugin() {
-	installPluginsTemplate("枫叶插件", "hs-qiqi-plugin", "git clone --depth=1  https://gitee.com/kesally/hs-qiqi-cv-plugin.git  ./plugins/hs-qiqi-plugin")
+	installPluginsTemplate("枫叶插件", "hs-qiqi-plugin", "git clone --depth=1  https://gitee.com/kesally/hs-qiqi-cv-plugin.git  ./plugins/hs-qiqi-plugin/")
 }
 
 func installPluginsTemplate(pluginChineseName string, dirName string, command ...string) {
