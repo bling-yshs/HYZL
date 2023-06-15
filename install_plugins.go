0c594c547cfd7ebdf0964e9cd345a5b665c60cb6
bling-yshs <377178599@qq.com>
2023-06-15 18:10:20 +0800

    feat: 在内置的安装插件界面，显示各插件的原地址

--- install_plugins.go
+++ install_plugins.go
@@ -46,23 +46,23 @@ func installPluginsMenu() {
 //↓插件安装函数
 
 func installStarRailPlugin() {
-	installPluginsTemplate("星穹铁道插件", "StarRail-plugin", "git clone --depth=1 https://gitee.com/hewang1an/StarRail-plugin.git ./plugins/StarRail-plugin/")
+	installPluginsTemplate("星穹铁道插件 (https://gitee.com/hewang1an/StarRail-plugin)", "StarRail-plugin", "git clone --depth=1 https://gitee.com/hewang1an/StarRail-plugin.git ./plugins/StarRail-plugin/")
 }
 
 func installGuobaPlugin() {
-	installPluginsTemplate("锅巴插件", "Guoba-Plugin", "git clone --depth=1 https://gitee.com/guoba-yunzai/guoba-plugin.git ./plugins/Guoba-Plugin/", "pnpm install --no-lockfile --filter=guoba-plugin -w")
+	installPluginsTemplate("锅巴插件 (https://gitee.com/guoba-yunzai/guoba-plugin)", "Guoba-Plugin", "git clone --depth=1 https://gitee.com/guoba-yunzai/guoba-plugin.git ./plugins/Guoba-Plugin/", "pnpm install --no-lockfile --filter=guoba-plugin -w")
 }
 
 func installMiaoPlugin() {
-	installPluginsTemplate("喵喵插件", "miao-plugin", "git clone --depth 1 -b master https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/", "pnpm add image-size -w")
+	installPluginsTemplate("喵喵插件 (https://gitee.com/yoimiya-kokomi/miao-plugin)", "miao-plugin", "git clone --depth 1 -b master https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/", "pnpm add image-size -w")
 }
 
 func installXiaoyaoPlugin() {
-	installPluginsTemplate("逍遥插件", "xiaoyao-cvs-plugin", "git clone --depth=1 https://gitee.com/Ctrlcvs/xiaoyao-cvs-plugin.git ./plugins/xiaoyao-cvs-plugin/", "pnpm add promise-retry -w", "pnpm add superagent -w")
+	installPluginsTemplate("逍遥插件 (https://gitee.com/Ctrlcvs/xiaoyao-cvs-plugin.git)", "xiaoyao-cvs-plugin", "git clone --depth=1 https://gitee.com/Ctrlcvs/xiaoyao-cvs-plugin.git ./plugins/xiaoyao-cvs-plugin/", "pnpm add promise-retry -w", "pnpm add superagent -w")
 }
 
 func installFengyePlugin() {
-	installPluginsTemplate("枫叶插件", "hs-qiqi-plugin", "git clone --depth=1  https://gitee.com/kesally/hs-qiqi-cv-plugin.git  ./plugins/hs-qiqi-plugin/")
+	installPluginsTemplate("枫叶插件 (https://gitee.com/kesally/hs-qiqi-cv-plugin.git)", "hs-qiqi-plugin", "git clone --depth=1 https://gitee.com/kesally/hs-qiqi-cv-plugin.git ./plugins/hs-qiqi-plugin/")
 }
 
 func installPluginsTemplate(pluginChineseName string, dirName string, command ...string) {
