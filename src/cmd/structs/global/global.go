22b89d426d3b317e8ac9d2e3aff36a4c7f64f1ac
bling-yshs <377178599@qq.com>
2024-05-22 17:42:14 +0800

    fix: 不小心修改了版本号

--- src/cmd/structs/global/global.go
+++ src/cmd/structs/global/global.go
@@ -33,7 +33,7 @@ var Global = global{
 	YunzaiName:     yunzaiName(),
 	ProgramName:    programName(),
 	ProgramRunPath: programRunPath(),
-	ProgramVersion: "v0.2.27",
+	ProgramVersion: "v0.2.28",
 }
 
 func yunzaiName() string {
