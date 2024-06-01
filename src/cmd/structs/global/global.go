0c503283b79c925f48938b743258fd03a974fe12
bling-yshs <377178599@qq.com>
2024-06-01 11:14:09 +0800

    release: v0.2.43

--- src/cmd/structs/global/global.go
+++ src/cmd/structs/global/global.go
@@ -33,7 +33,7 @@ var Global = global{
 	YunzaiName:     yunzaiName(),
 	ProgramName:    programName(),
 	ProgramRunPath: programRunPath(),
-	ProgramVersion: "v0.2.42",
+	ProgramVersion: "v0.2.43",
 }
 
 func yunzaiName() string {
