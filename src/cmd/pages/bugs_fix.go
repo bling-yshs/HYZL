e3692b24f784cd4cedaa7b3e9ce75f9cb076601a
bling-yshs <377178599@qq.com>
2024-10-21 06:46:41 +0800

    feat: 添加 BUG修复->修复 桌面出现白色窗口(治标不治本)

--- src/cmd/pages/bugs_fix.go
+++ src/cmd/pages/bugs_fix.go
@@ -8,10 +8,12 @@ import (
 	"github.com/bling-yshs/HYZL/src/cmd/utils/cmd_utils"
 	"github.com/bling-yshs/HYZL/src/cmd/utils/http_utils"
 	"github.com/bling-yshs/HYZL/src/cmd/utils/input_utils"
+	"github.com/bling-yshs/HYZL/src/cmd/utils/io_utils"
 	"github.com/bling-yshs/HYZL/src/cmd/utils/menu_utils"
 	"github.com/bling-yshs/HYZL/src/cmd/utils/print_utils"
 	ct "github.com/daviddengcn/go-colortext"
 	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
 	"os"
 	"path"
 	"path/filepath"
@@ -31,6 +33,7 @@ func BugsFixMenu() {
 		{"修复 喵喵云崽监听报错(也就是sqlite3问题)", sqliteFix},
 		{"修复 ffmpeg 未安装", ffmpegFix},
 		{"修复 无法正确获取图片链接", fixGetImageLink},
+		{"修复 桌面出现白色窗口(治标不治本)", fixPuppeteerWhiteWindow},
 	}
 
 	for {
@@ -42,6 +45,88 @@ func BugsFixMenu() {
 		}
 		menu_utils.DealChoice(choice, options, false)
 	}
+
+}
+
+func fixPuppeteerWhiteWindow() {
+	// 原理，复制Yunzai-Bot/renderers/puppeteer/config_default.yaml为config.yaml，然后用yaml解析，修改里面的args数组，添加--window-position=-10000,-10000
+	defaultConfigPath := path.Join(yunzai.GetYunzai().Path, "renderers", "puppeteer", "config_default.yaml")
+	configPath := path.Join(yunzai.GetYunzai().Path, "renderers", "puppeteer", "config.yaml")
+	err := io_utils.CopyFile(defaultConfigPath, configPath)
+	if err != nil {
+		print_utils.PrintError(errors.Wrap(err, "错误描述：复制 config_default.yaml 失败！"))
+		return
+	}
+	// 开始修改 config.yaml
+	bytes, err := os.ReadFile(configPath)
+	if err != nil {
+		print_utils.PrintError(errors.Wrap(err, "错误描述：读取 config.yaml 失败！"))
+		return
+	}
+	var node yaml.Node
+	if err := yaml.Unmarshal(bytes, &node); err != nil {
+		print_utils.PrintError(errors.Wrap(err, "错误描述：解析 config.yaml 失败！"))
+		return
+	}
+	list := node.Content[0]
+	for index, each := range list.Content {
+		if each.Value == "args" {
+			// 找到 "args" 节点
+			argsNode := list.Content[index+1]
+			// 创建新的节点
+			newNode := &yaml.Node{
+				Kind:  yaml.ScalarNode,
+				Style: 0,
+				Tag:   "!!str",
+				Value: "--window-position=-10000,-10000",
+			}
+			// 追加到 "args" 节点的内容中
+			argsNode.Content = append(argsNode.Content, newNode)
+			break
+		}
+	}
+	// 将修改后的 Node 写回 config.yaml 文件
+	newData, err := yaml.Marshal(&node)
+	if err != nil {
+		print_utils.PrintError(errors.Wrap(err, "错误描述：序列化 config.yaml 失败！"))
+		return
+	}
+	err = os.WriteFile(configPath, newData, 0644)
+	if err != nil {
+		print_utils.PrintError(errors.Wrap(err, "错误描述：写回 config.yaml 失败！"))
+		return
+	}
+	print_utils.PrintWithEmptyLine("修复成功！")
+	// 找到 args 数组并追加 --window-position=-10000,-10000
+
+	//var config map[string]interface{}
+	//err = yaml.Unmarshal(bytes, &config)
+	//if err != nil {
+	//	print_utils.PrintError(errors.Wrap(err, "错误描述：解析 config.yaml 失败！"))
+	//	return
+	//}
+	//// 找到 args 数组并追加 --window-position=-10000,-10000
+	//if renderers, ok := config["args"].([]interface{}); ok {
+	//	renderers = append(renderers, "--window-position=-10000,-10000")
+	//	config["args"] = renderers
+	//} else {
+	//	print_utils.PrintError(errors.New("未找到 args 数组或类型错误"))
+	//	return
+	//}
+	//
+	//// 将修改后的配置重新序列化为 YAML 格式
+	//newData, err := yaml.Marshal(&config)
+	//if err != nil {
+	//	print_utils.PrintError(errors.Wrap(err, "错误描述：序列化 config.yaml 失败！"))
+	//	return
+	//}
+	//
+	//// 写回 config.yaml 文件
+	//err = ioutil.WriteFile(configPath, newData, 0644)
+	//if err != nil {
+	//	print_utils.PrintError(errors.Wrap(err, "错误描述：写回 config.yaml 失败！"))
+	//	return
+	//}
 
 }
 
