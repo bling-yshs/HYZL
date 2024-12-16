7f059bf61ebfc4c4b08e47867f06f3f910e16098
bling-yshs <377178599@qq.com>
2024-12-16 09:58:18 +0800

    refactor: 切换更新json和公告json到gitee

--- src/cmd/structs/app/app.go
+++ src/cmd/structs/app/app.go
@@ -34,8 +34,8 @@ func init() {
 		panic(err)
 	}
 	appInstance.ConfigDir = filepath.Join(appInstance.Workdir, "config")
-	appInstance.UpdateUrl = "https://hyzl.r2.yshs.fun/updater/updater.json"
-	appInstance.AnnouncementUrl = "https://hyzl.r2.yshs.fun/announcement/announcement.json"
+	appInstance.UpdateUrl = "https://gitee.com/bling_yshs/HYZL-updater/raw/main/updater.json"
+	appInstance.AnnouncementUrl = "https://gitee.com/bling_yshs/HYZL-announcement/raw/main/announcement.json"
 }
 
 func GetApp() app {
